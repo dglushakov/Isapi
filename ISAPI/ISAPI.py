e4a339dc4d206a7c1ab75fc1f2d7c0867eee5753
Denis Glushakov <denis.glushakov@bk.ru>
2025-01-15 14:24:22 +0300

    initial

--- ISAPI/ISAPI.py
+++ ISAPI/ISAPI.py
@@ -5,24 +5,14 @@ import xml.etree.ElementTree as ET
 
 
 class ISAPI:
-    @staticmethod
-    def is_request_has_successs(response):
-        if response["success"] == 0 or response["error"] or response["status_code"] != 200:
-            return True
-        return False
-
-    ISAPI_PATHS = {
+    ISAPI_GET_PATHS = {
         "DEVICE_INFO": "/ISAPI/System/deviceinfo",
         "USERS": "/ISAPI/Security/users",
-
         "CHANNELS_STATUS": "/ISAPI/System/workingstatus/chanStatus?format=json",
         "CHANNELS": "/ISAPI/ContentMgmt/InputProxy/channels",
-
-        "working_status": "/ISAPI/System/workingstatus",
-
-        "hdd": "/ISAPI/ContentMgmt/Storage/hdd",
-
-        "status": "/ISAPI/System/Video/capabilities",  # ?
+        "WORKING_STATUS": "/ISAPI/System/workingstatus",
+        "HDD_STATUS": "/ISAPI/ContentMgmt/Storage/hdd",
+        "STATUS": "/ISAPI/System/Video/capabilities",
     }
 
     def __init__(self, ip, login="admin", password="12345", protocol="http"):
@@ -31,82 +21,37 @@ class ISAPI:
         self.password = password
         self.protocol = protocol
 
-    def __make_url__(self, isapi_abs_path: str) -> str:
-        url = self.protocol + "://" + self.ip + isapi_abs_path
-        return url
-
-    def xml_to_dict_recursive(self, root):
-
-        trash_perfix = "{http://www.hikvision.com/ver20/XMLSchema}"
-
-        if root.tag.startswith(trash_perfix):
-            tag = root.tag[len(trash_perfix):]
-        else:
-            tag = root.tag
-        if len(list(root)) == 0:
-            return {tag: root.text}
-        else:
-            return {tag: list(map(self.xml_to_dict_recursive, list(root)))}
-
-    def get_request(self, isapi_abs_path):
-        url = self.__make_url__(isapi_abs_path)
+    def get_request(self, isapi_abs_path_key):
+        url = self.protocol + "://" + self.ip + self.ISAPI_GET_PATHS[isapi_abs_path_key] + "?format=json"
         response = {
+            "url": url,
             "success": 0,
             "error": None,
             "status_code": None,
             "text": None,
         }
         try:
-            r = requests.get(url, auth=HTTPDigestAuth(self.login, self.password), timeout=1)
+            headers = {'Content-type': 'application/json'}
+            r = requests.get(url, auth=HTTPDigestAuth(self.login, self.password), timeout=1, headers=headers)
+            # r = requests.get(url, auth=HTTPDigestAuth(self.login, self.password), timeout=1,)
             response["success"] = 1
             response["status_code"] = r.status_code
             response["text"] = r.text
         except requests.exceptions.ConnectTimeout as error:
+            response["success"] = 0
             response["error"] = error
         finally:
-            return response
-
-    def post_request(ip, isapi_abs_path, payload, login="admin", password="2wsx#EDC1", protocol="http"):
-        pass
-
-    def get_users(self):
-        """ Возвращает список словарей вида: {'id': '1', 'userName': 'admin', 'userLevel': 'Administrator'} или None (в случае ошибки)"""
-        response_xml = self.get_request(self.ISAPI_PATHS["USERS"])
-        if ISAPI.is_request_has_successs(response_xml):
-            return None
-        else:
-            result = []
-            root = ET.XML(response_xml["text"])
-            users_data = self.xml_to_dict_recursive(root)
-
-            for user in users_data["UserList"]:
-                id = user["User"][0]["id"]
-                username = user["User"][1]["userName"]
-                userlevel = user["User"][4]["userLevel"]
-                result.append({
-                    "id": id,
-                    "userName": username,
-                    "userLevel": userlevel
-                })
-
-            return result
-
-    def get_device_info(self):
-        response_xml = self.get_request(self.ISAPI_PATHS["DEVICE_INFO"])
-        if ISAPI.is_request_has_successs(response_xml):
-            return None
-        else:
-            root = ET.XML(response_xml["text"])
-            response = self.xml_to_dict_recursive(root)
             return response
 
 
 if __name__ == '__main__':
     isapi = ISAPI(ip="192.168.2.168", login='admin', password='2wsx#EDC')
-    users = isapi.get_users()
-    print(users)
-    if users:
-        for user in users:
-            print(user)
 
-    print(isapi.get_device_info())
+    print(isapi.get_request(isapi_abs_path_key="HDD_STATUS"))
+    print(isapi.get_request(isapi_abs_path_key="HDD_STATUS")["text"])
+    print(isapi.get_request(isapi_abs_path_key="DEVICE_INFO")["text"])
+    print(isapi.get_request(isapi_abs_path_key="WORKING_STATUS")["text"])
+
+    print("=================")
+    print(isapi.get_request(isapi_abs_path_key="USERS")["url"])
+    print(isapi.get_request(isapi_abs_path_key="USERS")["text"])
